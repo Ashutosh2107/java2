0a45d73900abbb9dc5939a2bf22e227d5c967e5b
shiva <shivachouksey2107@gmail.com>
2025-11-19 16:16:46 +0530

    Initial commit

---
+++ src/c10.java
@@ -0,0 +1,40 @@
+//class Animal{
+//    void eat(){
+//        System.out.println("pizza");
+//    }
+//    void data(){
+//        System.out.println("hii");
+//    }
+//}
+//class Dog extends  Animal{
+//    void eat(){
+//        System.out.println("biryani");
+//
+//    }
+//    void sleep(){
+//        System.out.println("hello");
+//
+//    }
+//}
+//class main{
+//    public static void main(String[] args){
+//Dog s1=new Dog();
+//s1.eat();
+//}
+//}
+
+//abstractions
+abstract class Animal{
+abstract void eat ();
+}
+class Cat extends Animal {
+void eat(){
+System.out.println("kitkat");
+}
+}
+class  main{
+public static void  main(String[] args){
+Cat s1 =new Cat();
+s1.eat();
+}
+}
